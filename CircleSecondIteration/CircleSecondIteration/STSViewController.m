d63e8855e903da1b3e269e93a3accfc601b2216e
Matt Chiang <mattchiang777@gmail.com>
2014-06-03 02:16:55 -0700

    Added tutorial touch pulse

--- CircleSecondIteration/CircleSecondIteration/STSViewController.m
+++ CircleSecondIteration/CircleSecondIteration/STSViewController.m
@@ -8,6 +8,7 @@
 
 #import "STSViewController.h"
 #import "STSWelcomeScene.h"
+#import "STSHeroRotationScene.h"
 
 @implementation STSViewController
 
@@ -19,13 +20,20 @@
     // skView.showsFPS = YES;
     // skView.showsNodeCount = YES;
     // skView.showsPhysics = YES;
-    
+
+//    // Use NSUserDefaults to launch tutorial only once ****YUJUN: UNCOMMENT TO GO STRAIGHT TO TUTORIAL TESTING****
+//    STSHeroRotationScene *heroRotationScene = [[STSHeroRotationScene alloc] initWithSize:skView.bounds.size];
+//    heroRotationScene.scaleMode = SKSceneScaleModeAspectFill;
+//    [skView presentScene:heroRotationScene];
+
+
     // Create and configure the scene.
     STSWelcomeScene *scene = [[STSWelcomeScene alloc] initWithSize:skView.bounds.size];
     scene.scaleMode = SKSceneScaleModeAspectFill;
     
     // Present the scene.
     [skView presentScene:scene];
+
 }
 
 - (BOOL)shouldAutorotate {
